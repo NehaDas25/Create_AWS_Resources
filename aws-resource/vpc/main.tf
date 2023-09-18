3b0fb09dc7bdbdc01b0abb0316e0079dcb3e58e9
Neha Das <nehadas9619@gmail.com>
2023-09-18 16:08:32 -0700

    Add all terraform files

---
+++ aws-resource/vpc/main.tf
@@ -0,0 +1,111 @@
+###########################################################
+                     VPC - CIDR Block
+###########################################################
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr_block
+
+  tags = {
+    Name = "${var.vpc_name}-VPC"
+  }
+}
+
+###########################################################
+                 VPC - Internet Gateway
+###########################################################
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vpc_name} IG"
+  }
+}
+
+###########################################################
+                    Public Subnets CIDRs
+###########################################################
+resource "aws_subnet" "public_subnets" {
+  count             = length(var.azs)
+  vpc_id            = aws_vpc.main.id
+  availability_zone = element(var.azs, count.index)
+  cidr_block        = element(var.public_subnet_cidrs, count.index)
+
+  tags = {
+    Name = "Public Subnet ${count.index + 1}"
+  }
+}
+
+###########################################################
+                    Private Subnets CIDRs
+###########################################################
+resource "aws_subnet" "private_subnets" {
+  count             = length(var.azs)
+  vpc_id            = aws_vpc.main.id
+  availability_zone = element(var.azs, count.index)
+  cidr_block        = element(var.private_subnet_cidrs, count.index)
+
+  tags = {
+    Name = "Private Subnet ${count.index + 1}"
+  }
+}
+
+###########################################################
+               Elastic IP for NAT GW
+###########################################################
+resource "aws_eip" "EIP_Nat_GW" {
+  count = length(aws_subnet.private_subnets)
+  vpc   = true
+}
+
+###########################################################
+                         Nat Gateway
+###########################################################
+resource "aws_nat_gateway" "Nat_Gateways" {
+  count         = length(aws_subnet.private_subnets)
+  allocation_id = element(aws_eip.EIP_Nat_GW, count.index).id
+  subnet_id     = element(aws_subnet.private_subnets, count.index).id
+}
+
+###########################################################
+                    Public Subnet Route Table
+###########################################################
+resource "aws_route_table" "public_subnets_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "Public Subnet Route Table"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_asso" {
+  count          = length(aws_subnet.public_subnets)
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.public_subnets_rt.id
+}
+
+###########################################################
+                    Private Subnet Route Table
+###########################################################
+resource "aws_route_table" "private_subnets" {
+  count  = length(aws_nat_gateway.Nat_Gateways)
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = element(aws_nat_gateway.Nat_Gateways, count.index).id
+  }
+
+  tags = {
+    Name = "Private Subnet Route Table"
+  }
+}
+
+resource "aws_route_table_association" "private_subnet_asso" {
+  count          = length(aws_subnet.private_subnets)
+  subnet_id      = element(aws_subnet.private_subnets[*].id, count.index)
+  route_table_id = element(aws_route_table.private_subnets[*].id, count.index)
+}
