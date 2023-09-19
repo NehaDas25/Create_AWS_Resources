7f2e20040665fd650c4c0d61b7b882c4946e971a
Neha Das <nehadas9619@gmail.com>
2023-09-18 18:14:35 -0700

    Add destroy to yaml

--- aws-resource/vpc/main.tf
+++ aws-resource/vpc/main.tf
@@ -3,6 +3,9 @@
 ###########################################################
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr_block
+  enable_dns_support = "true"
+  enable_dns_hostnames = "true"
+  instance_tenancy = "default"
 
   tags = {
     Name = "${var.basename}-VPC"
